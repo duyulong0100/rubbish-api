a55e644a1dca65f391c21bac637b1f184eb7195b
Yulong Du <duyulong@le.com>
2019-08-01 11:07:06 +0800

    code init

--- src/main/java/com/xserver/config/WebMvcConfigurer.java
+++ src/main/java/com/xserver/config/WebMvcConfigurer.java
@@ -39,6 +39,6 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowCredentials(true)
-                .allowedMethods("GET", "POST", "DELETE", "PUT").maxAge(3600);
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS").maxAge(3600);
     }
 }
